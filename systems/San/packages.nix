a8bf05b234b0b5bda0ebd79b8d53940171076d95
Svenum <s.ziegler@holypenguin.net>
2023-06-27 16:36:59 +0200

    add steam

--- systems/San/packages.nix
+++ systems/San/packages.nix
@@ -7,6 +7,12 @@
       rsync
       timeshift
       nvtop
+      steam
     ];
+  };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 }
