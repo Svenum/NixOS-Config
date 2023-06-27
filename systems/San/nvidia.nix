8912f9f98e396a961b35c932c8ea6163170b8240
Svenum <s.ziegler@holypenguin.net>
2023-06-27 16:16:55 +0200

    add prime offload

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -8,16 +8,15 @@
       powerManagement.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
       prime = {
-      #  offload = {
-      #    enable = true;
-      #    enableOffloadCmd = true;
-      #  };
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         reverseSync.enable = true;
         amdgpuBusId = "PCI:5:0:0";
         nvidiaBusId = "PCI:1:0:0";
       };
     };
   };
-
   services.xserver.videoDrivers = [ "nvidia" ];
 }
