2b1ffa2f9ef9d7e7dc1197e2b0886fc15489eb0a
Svenum <s.ziegler@holypenguin.net>
2023-06-27 15:53:08 +0200

    enable reverse sync

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -2,16 +2,17 @@
 
 {
   hardware = {
-    opengl.enable = true;
+    #opengl.enable = true;
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
+      #prime = {
+      #  offload = {
+      #    enable = true;
+      #    enableOffloadCmd = true;
+      #  };
+      reverseSync.enable = true;
         amdgpuBusId = "PCI:5:0:0";
         nvidiaBusId = "PCI:1:0:0";
       };
