d37a92ff6b646bbdd7bdcd6af2a880608dcf5797
Svenum <s.ziegler@holypenguin.net>
2023-06-29 21:23:23 +0200

    remove tuxedo

--- flake.nix
+++ flake.nix
@@ -19,15 +19,9 @@
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    tuxedo-nixos = {
-      url = "github:blitz/tuxedo-nixos";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, tuxedo-nixos, ... }:
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     # Specific system configurations
     let 
     	defaultModules = [
