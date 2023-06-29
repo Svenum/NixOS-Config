f1570d3b81ead64abc0e366cd30e0fe9d8bffffc
Svenum <s.ziegler@holypenguin.net>
2023-06-29 20:46:14 +0200

    fix typo

--- systems/San/tlp.nix
+++ systems/San/tlp.nix
@@ -15,7 +15,7 @@
       RADEON_DPM_ON_DC = "battery";
       WOL_DISABLE = "Y";
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersafe";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
       CPU_HWP_DYN_BOOST_ON_AC = 1;
