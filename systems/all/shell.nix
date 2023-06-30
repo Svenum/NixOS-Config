d2a4c64ba55e621baaeba2a84fa79b9f28441eb1
Svenum <s.ziegler@holypenguin.net>
2023-06-30 17:38:43 +0200

    set default shell

--- systems/all/shell.nix
+++ systems/all/shell.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.zsh = {
     enable = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
+    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+    };
   };
 }
--- systems/all/user.nix
+++ systems/all/user.nix
@@ -1,14 +1,17 @@
 { pkgs, ... }:
 
 {
+  users.defaultUserShell = pkgs.zsh;
   users.users.susven = {
     isNormalUser = true;
     description = "Susven";
     extraGroups = [ "networkmanager" "wheel" ];
+    useDefaultShell = true;
   };
   users.users.sven = {
     isNormalUser = true;
     description = "Sven";
     extraGroups = [ "networkmanager" "libvirtd" ];
+    useDefaultShell = true;
   };
 }
