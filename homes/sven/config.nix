8c43c46bcd30adab266f8b590a564673926447c5
Svenum <s.ziegler@holypenguin.net>
2023-07-01 17:18:42 +0200

    update config files

--- homes/sven/config.nix
+++ homes/sven/config.nix
@@ -1,6 +1,19 @@
 { ... }:
 
 {
-  home.file.".zshrc".source = ../../config_files/zshrc;
-  home.file.".p10k.zsh".source = ../../config_files/p10k.zsh;
+  home.file = {
+    # Zsh
+    ".zshrc".source = ../../config_files/zshrc;
+    ".p10k.zsh".source = ../../config_files/p10k.zsh;
+    # KDE 
+    ".config/kdedefaults/ksplashrc".source = ../../config_files/config/kdedefaults/ksplashrc;
+    ".config/kdedefaults/kscreenlockerrc".source = ../../config_files/config/kdedefaults/kscreenlockerrc;
+    ".config/kdedefaults/kdeglobals".source = ../../config_files/config/kdedefaults/kdeglobals;
+    ".config/kdedefaults/plasmarc".source = ../../config_files/config/kdedefaults/plasmarc;
+    ".config/kcminputrc".source = ../../config_files/config/kcminputrc;
+    ".config/kwinrc".source = ../../config_files/config/kwinrc;
+    ".config/kdeglobals".source = ../../config_files/config/kdeglobals;
+    # Flatapk overrides
+    ".local/share/flatpak/overrides".source = ../../config_files/local/share/flatpak/overrides;
+  };
 }
