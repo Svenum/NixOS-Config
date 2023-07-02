5b300d8c1152507c8c705d1de049f7307e6376f7
Svenum <s.ziegler@holypenguin.net>
2023-07-02 14:15:58 +0200

    Add files

--- homes/sven/packages.nix
+++ homes/sven/packages.nix
@@ -3,6 +3,5 @@
 {
   home.packages = with pkgs; [
     caffeine-ng
-    latte-dock
   ];
 }
--- systems/San/fs.nix
+++ systems/San/fs.nix
@@ -2,15 +2,13 @@
 
 {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ac15d02e-692d-49ed-be80-17764460bc84";
-      fsType = "btrfs";
-      options = [ "subvol=@" ];
+    { device = "/dev/disk/by-uuid/fedb4e6c-ce61-4d4c-b213-7c2d14444cba";
+      fsType = "ext4";
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/c29aff97-741f-4515-9617-29384227a21d";
-      fsType = "btrfs";
-      options = [ "subvol=@home" ];
+    { device = "/dev/disk/by-uuid/a4b2f370-b508-4a30-8e5d-fd3651e809c5";
+      fsType = "ext4";
     };
 
   fileSystems."/boot" =
