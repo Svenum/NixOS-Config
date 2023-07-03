9f217e6530c028b75538ce41b150a8fa2294a278
Svenum <s.ziegler@holypenguin.net>
2023-07-03 22:36:14 +0200

    add flatpak

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, lib, ... }:
+{ stdenv, pkgs, config, lib, ... }:
 
 with lib;
 let
@@ -7,17 +7,29 @@ in
 {
   options.flatpak = {
     enable = mkEnableOption "Flatpak";
+    system = {
+      enable = {
+        types = mkBooleanOption;
+        default = false;
+      };
+      repos = {
+        types = types.Of types.str;
+        default = [ "https://flathub.org/repo/flathub.flatpakrepo" ];
+      };
+      packages = {
+        types = types.Of types.str;
+        default = [];
+      };
+    };
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable{
 
     # Enable Flatpak
-    services.enable.flatpak = mkForce true;
+    services.flatpak.enable = mkForce true;
     xdg.portal = mkForce {
       enable = true;
       extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     };
-
-
   };
 }
