721d4512235a03f2321c7b517b3ca71d54fe1609
Svenum <s.ziegler@holypenguin.net>
2023-07-07 15:04:58 +0200

    add deps

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -9,13 +9,15 @@ in
 
   # Copy items
   system.userActivationScripts = {
-    copySkelDir = ''
-      for user in ${toString users}; do
-        if [ ! -f /home/$user/.skel.lock ]; then
-          cp -r /etc/skel/* /home/$user/ && touch /home/$user/.skel.lock
-          chmod -R 640 /home/$user/
-        fi
-      done
-    '';
+    copySkelDir = {
+      text = ''
+        for user in ${toString users}; do
+          if [ ! -f /home/$user/.skel.lock ]; then
+            cp -r /etc/skel/* /home/$user/ && touch /home/$user/.skel.lock
+            chmod -R 640 /home/$user/
+          fi
+        done
+      '';
+      deps = [ environment.etc ];
   };
 }
