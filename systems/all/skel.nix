c1fcf4a32eb941d35264947477e1aa3642cbadcc
Svenum <s.ziegler@holypenguin.net>
2023-07-07 15:15:47 +0200

    change script

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -13,7 +13,7 @@ in
       text = ''
         for user in ${toString users}; do
           if [ ! -f /home/$user/.skel.lock ]; then
-            cp -r /etc/skel/* /home/$user/ && touch /home/$user/.skel.lock
+            cp -r /etc/skel/. /home/$user/ && touch /home/$user/.skel.lock
             chmod -R 640 /home/$user/
           fi
         done
