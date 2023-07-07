ca91ddd0ffcb868a56ccf0bce1de8bbbe297ada7
Svenum <s.ziegler@holypenguin.net>
2023-07-07 14:20:24 +0200

    change mode

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -2,6 +2,6 @@
 
 {
   environment.etc."skel".source = ../../skel;
-  environment.etc."skel".mode = "0640";
+  environment.etc."skel".mode = "0600";
   security.pam.makeHomeDir.skelDirectory = "/etc/skel";
 }
