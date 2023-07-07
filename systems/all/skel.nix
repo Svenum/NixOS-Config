78a210b29914ae3b76a960594b3490778e18114f
Svenum <s.ziegler@holypenguin.net>
2023-07-07 14:58:33 +0200

    add skel things

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -1,6 +1,21 @@
-{ ... }:
-
+{ pkgs, ... }:
+let
+  users = [ "sven", "susven" ];
+in
 {
+  # Create Dir
   environment.etc."skel".source = ../../skel;
   security.pam.makeHomeDir.skelDirectory = "/etc/skel";
+
+  # Copy items
+  system.userActivationScripts = {
+    copySkelDir = ''
+      for user in ${toString users}; do
+        if [ ! -f /home/$user/.skel.lock ]; then
+          cp /etc/skel/* /home/$user/ && touch /home/$user/.skel.lock
+          chmod -R 640 /home/$user/
+        fi
+      done
+    '';
+  };
 }
