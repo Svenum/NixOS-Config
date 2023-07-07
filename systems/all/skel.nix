7696283c077b1e7ed277e1501acbc7d9261eb9a5
Svenum <s.ziegler@holypenguin.net>
2023-07-07 14:17:31 +0200

    add skel

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -1,9 +1,7 @@
 { ... }:
 
 {
-  environment.etc."skel" = {
-    source = ../../skel;
-    mode = "0640";
-  };
+  environment.etc."skel".source = ../../skel;
+  environment.etc."skel".mode = "0640";
   security.pam.makeHomeDir.skelDirectory = "/etc/skel";
 }
