a3c6e40ffdffc929b4a8c22c6911a000fd81ae35
Svenum <s.ziegler@holypenguin.net>
2023-07-07 15:19:17 +0200

    add root

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -14,10 +14,14 @@ in
         for user in ${toString users}; do
           if [ ! -f /home/$user/.skel.lock ]; then
             cp -r /etc/skel/. /home/$user/ && touch /home/$user/.skel.lock
-            chown -R $user /home/$user/
+            chown -R $user:users /home/$user/
             chmod -R 640 /home/$user/
           fi
         done
+        if [ ! -f /home/$user/.skel.lock ]; then
+          cp -r /etc/skel/. /root/ && touch /root/.skel.lock
+          chmod -R 640 /root/
+        fi
       '';
       deps = [ ];
     };
