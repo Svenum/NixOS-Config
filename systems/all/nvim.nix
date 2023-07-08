f9b312b67827e901e9302cddf6b8d7c964a290c9
Svenum <s.ziegler@holypenguin.net>
2023-07-08 12:35:32 +0200

    add coc config

--- systems/all/nvim.nix
+++ systems/all/nvim.nix
@@ -12,6 +12,19 @@
         " Airline
         let g:airline_theme='luna'
 
+        " Coc
+        set nobackup
+        set nowriteback
+        set updatetime=300
+        set signcolumn=yes
+        function! CheckBackspace() abort
+          let col = col('.') - 1
+          return !col || getline('.')[col -1] =~# '\s'
+        endfunction
+
+        inoremap <silent><expr> <A-CR> coc#pum#visible() ? coc#pum#confirm()
+                  \: "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"
+
       '';
       packages.nix = {
         start = with pkgs.vimPlugins; [
