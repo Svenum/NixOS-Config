e350893948d93b3a22ad1474e9d4bb9e452c41c4
Svenum <s.ziegler@holypenguin.net>
2023-07-09 12:05:17 +0200

    add hostname var

--- flake.nix
+++ flake.nix
@@ -53,8 +53,7 @@
     nixosConfigurations = {
       srv-nixostest = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs;
-        hostname = "srv-nixostest";
+        specialArgs = inputs + hostname = "srv-nixostest";
         modules = defaultModules ++ [
 	  # System
           ./systems/srv-nixostest/default.nix
@@ -62,8 +61,7 @@
       };
       Ni = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs;
-        hostname = "Ni";
+        specialArgs = inputs + hostname = "Ni";
         modules = defaultModules ++ [
 	  # System
           ./systems/Ni/default.nix
@@ -71,8 +69,7 @@
       };
       San = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = inputs;
-        hostname = "San";
+        specialArgs = inputs + hostname = "San";
         modules = defaultModules ++ [
 	  # System
           ./systems/San/default.nix
