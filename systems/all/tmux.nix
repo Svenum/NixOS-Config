6f34f3fbce3197f93e3b8dff69bff3c4ea54e9af
Svenum <s.ziegler@holypenguin.net>
2023-07-09 18:14:26 +0200

    enable scrolling in tmux

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -7,6 +7,9 @@
     keyMode = "vi";
     historyLimit = 5000;
     clock24 = true;
+    extraConfig = ''
+      set -g mouse on
+    '';
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
       better-mouse-mode
