9cf8a5b1c79d75f854845836a6bc35e68c304adb
Svenum <s.ziegler@holypenguin.net>
2023-07-09 18:55:36 +0200

    update tmux theme

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -10,6 +10,11 @@
     terminal = "screen-256color";
     extraConfig = ''
       set -g mouse on
+      set -g @catppuccin_powerline_icons_theme_enabled on
+      set -g @catppuccin_l_left_separator ""
+      set -g @catppuccin_l_right_separator ""
+      set -g @catppuccin_r_left_separator ""
+      set -g @catppuccin_r_right_separator ""
     '';
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
