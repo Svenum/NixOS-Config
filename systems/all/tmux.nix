1f7f3a42b5b905ee5ef9c9ace5c2ecff2d6b8e1e
Svenum <s.ziegler@holypenguin.net>
2023-07-10 18:42:26 +0200

    update tmux config

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -10,6 +10,9 @@
     terminal = "screen-256color";
     extraConfig = ''
       set -g mouse on
+      set -g @catppuccin_host "on"
+      set -g @catppuccin_user "on"
+      set -g @catppuccin_date_time "%d-%m-%Y %H:%M"
       set -g @catppuccin_powerline_icons_theme_enabled "on"
       set -g @catppuccin_l_left_separator ""
       set -g @catppuccin_l_right_separator ""
