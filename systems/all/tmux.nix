495eed6cc93cfb4256f9a3d618c2b33bd53b404c
Svenum <s.ziegler@holypenguin.net>
2023-07-10 18:33:23 +0200

    load config

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -10,14 +10,11 @@
     terminal = "screen-256color";
     extraConfig = ''
       set -g mouse on
-      set -g @catppuccin_flavour 'latte'
-      set -g @catppuccin_powerline_icons_theme_enabled on
+      set -g @catppuccin_powerline_icons_theme_enabled "on"
       set -g @catppuccin_l_left_separator ""
       set -g @catppuccin_l_right_separator ""
       set -g @catppuccin_r_left_separator ""
       set -g @catppuccin_r_right_separator ""
-
-      source-file /etc/tmux.conf
     '';
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
