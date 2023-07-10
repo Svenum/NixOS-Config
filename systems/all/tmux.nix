923436d31e6b8237f2a67ca1dfdddf1aac93caac
Svenum <s.ziegler@holypenguin.net>
2023-07-10 18:48:38 +0200

    add -g

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -10,14 +10,14 @@
     terminal = "screen-256color";
     extraConfig = ''
       set -g mouse on
-      set @catppuccin_host "on"
-      set @catppuccin_user "on"
-      set @catppuccin_date_time "%d-%m-%Y %H:%M"
-      set @catppuccin_powerline_icons_theme_enabled "on"
-      set @catppuccin_l_left_separator ""
-      set @catppuccin_l_right_separator ""
-      set @catppuccin_r_left_separator ""
-      set @catppuccin_r_right_separator ""
+      set -g @catppuccin_host "on"
+      set -g @catppuccin_user "on"
+      set -g @catppuccin_date_time "%d-%m-%Y %H:%M"
+      set -g @catppuccin_powerline_icons_theme_enabled "on"
+      set -g @catppuccin_l_left_separator ""
+      set -g @catppuccin_l_right_separator ""
+      set -g @catppuccin_r_left_separator ""
+      set -g @catppuccin_r_right_separator ""
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
     '';
