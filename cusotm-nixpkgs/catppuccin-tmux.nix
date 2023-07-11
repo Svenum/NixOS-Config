63de9c95ffbdf2e5a66e29c3bed67fb7cd45a93b
Svenum <s.ziegler@holypenguin.net>
2023-07-11 17:57:46 +0200

    debug

--- cusotm-nixpkgs/catppuccin-tmux.nix
+++ cusotm-nixpkgs/catppuccin-tmux.nix
@@ -1,53 +1,9 @@
 { pkgs, stdenv, lib, fetchFromGitHub }:
-let
-  rtpPath = "share/tmux-plugins";
-
-  addRtp = path: rtpFilePath: attrs: derivation:
-    derivation // { rtp = "${derivation}/${path}/${rtpFilePath}"; } // {
-      overrideAttrs = f: mkTmuxPlugin (attrs // f attrs);
-    };
-
-  mkTmuxPlugin = a@{
-    pluginName,
-    rtpFilePath ? (builtins.replaceStrings ["-"] ["_"] pluginName) + ".tmux",
-    namePrefix ? "tmuxplugin-",
-    src,
-    unpackPhase ? "",
-    configurePhase ? ":",
-    buildPhase ? ":",
-    addonInfo ? null,
-    preInstall ? "",
-    postInstall ? "",
-    path ? lib.getName pluginName,
-    ...
-  }:
-    if lib.hasAttr "dependencies" a then
-      throw "dependencies attribute is obselete. see NixOS/nixpkgs#118034" # added 2021-04-01
-    else addRtp "${rtpPath}/${path}" rtpFilePath a (stdenv.mkDerivation (a // {
-      pname = namePrefix + pluginName;
-
-      inherit pluginName unpackPhase configurePhase buildPhase addonInfo preInstall postInstall;
-
-      installPhase = ''
-        runHook preInstall
-
-        target=$out/${rtpPath}/${path}
-        mkdir -p $out/${rtpPath}
-        cp -r . $target
-        if [ -n "$addonInfo" ]; then
-          echo "$addonInfo" > $target/addon-info.json
-        fi
-
-        runHook postInstall
-      '';
-    }));
 
 in rec {
-  inherit mkTmuxPlugin;
-
   mkDerivation = throw "tmuxPlugins.mkDerivation is deprecated, use tmuxPlugins.mkTmuxPlugin instead"; # added 2021-03-14
 
-  catppuccin = mkTmuxPlugin {
+  catppuccin = tmuxPlugins.mkTmuxPlugin {
     pluginName = "catppuccin";
     version = "unstable-2023-04-03";
     src = fetchFromGitHub {
