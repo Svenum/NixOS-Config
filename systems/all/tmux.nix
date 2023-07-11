292f45647911d607b2bd64f75d5e5f9f35e4d62c
Svenum <s.ziegler@holypenguin.net>
2023-07-11 18:20:06 +0200

    fix typo

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -9,7 +9,7 @@
     clock24 = true;
     terminal = "screen-256color";
     plugins = with pkgs.tmuxPlugins; [
-      (with pkgs; callPackage ../../custom-nixpkgs/catppuccin-tmux { })
+      (import ../../custom-nixpkgs/catppuccin-tmux)
     ];
   };
 }
