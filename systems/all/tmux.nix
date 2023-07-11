89d21dd79242b95c10f8981fe0a526ba3f52264d
Svenum <s.ziegler@holypenguin.net>
2023-07-11 21:10:57 +0200

    debug

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -9,7 +9,7 @@
     clock24 = true;
     terminal = "screen-256color";
     plugins = with pkgs.tmuxPlugins; [
-      (with import <nixpkgs>; callPackage ../../custom-nixpkgs/catppuccin-tmux {})
+      (with pkgs; callPackage ../../custom-nixpkgs/catppuccin-tmux {})
     ];
   };
 }
