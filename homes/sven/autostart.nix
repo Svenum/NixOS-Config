51c7ffe6d7c606f65345d43fdf99b33f93b7013f
Svenum <s.ziegler@holypenguin.net>
2023-07-16 12:48:23 +0200

    test

--- homes/sven/autostart.nix
+++ homes/sven/autostart.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 
 {
-  xdg.configFile."autostart/solaar.desktop".source = mkif pathExists "${pkgs.solaar}/share/applications/solaar.desktop" "${pkgs.solaar}/share/applications/solaar.desktop";
-  xdg.configFile."autostart/brave.desktop".source = mkif pathExists "${pkgs.brave}/share/applications/solaar.desktop" "${pkgs.brave}/share/applications/solaar.desktop";
+  xdg.configFile."autostart/solaar.desktop".source = mkIf pathExists "${pkgs.solaar}/share/applications/solaar.desktop" "${pkgs.solaar}/share/applications/solaar.desktop";
+  xdg.configFile."autostart/brave.desktop".source = mkIf pathExists "${pkgs.brave}/share/applications/solaar.desktop" "${pkgs.brave}/share/applications/solaar.desktop";
 }
