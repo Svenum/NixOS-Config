f044be5eb08c7dc591da10de79ae838e418e8744
Svenum <s.ziegler@holypenguin.net>
2023-07-16 12:34:05 +0200

    debug

--- homes/sven/autostart.nix
+++ homes/sven/autostart.nix
@@ -1,5 +1,5 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 
 {
-  home.file."autostart/solaar.desktop".source = lib.mkIf pkgs.solaar "${pkgs.solaar}/share/applications/solaar.desktop";
+  home.file."autostart/solaar.desktop".source = "${pkgs.solaar}/share/applications/solaar.desktop";
 }
