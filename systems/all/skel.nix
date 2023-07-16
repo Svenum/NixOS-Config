04faffd1e8c8f89cbc9f7be64085915e295fc70b
Svenum <s.ziegler@holypenguin.net>
2023-07-16 12:00:25 +0200

    fix bug

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -7,7 +7,7 @@ in
   security.pam.makeHomeDir.skelDirectory = "/etc/skel";
 
   # Copy items
-  system.activationScripts = lib.stringAfter environment.etc.skel.source {
+  system.activationScripts = lib.stringAfter "environment.etc.skel.source" {
     copySkelDir = {
       text = ''
         for user in ${toString users}; do
