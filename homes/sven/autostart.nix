79e9aea5e4fc68fa90bdfa17718595b0b8c41124
Svenum <s.ziegler@holypenguin.net>
2023-07-16 12:27:00 +0200

    debug

--- homes/sven/autostart.nix
+++ homes/sven/autostart.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }
+{ pkgs, ... }:
 
 {
   home.file."autostart/solaar.desktop".source = builtins.elem pkgs.solaar environment.systemPackages "${pkgs.solaar}/share/applications/solaar.desktop";
