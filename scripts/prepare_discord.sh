128f50f81f31a000ff0052c4adeead4a0c013eda
Svenum <s.ziegler@holypenguin.net>
2023-07-17 20:49:43 +0200

    add prepare discord

--- scripts/prepare_discord.sh
+++ scripts/prepare_discord.sh
@@ -5,3 +5,8 @@ flatpak install --user com.discordapp.Discord -y
 # install betterdiscord 
 echo "install betterdiscord ..."
 betterdiscordctl -i flatpak install
+
+echo "
+/* @import url("https://catppuccin.github.io/discord/dist/catppuccin-latte-teal.theme.css"); */
+/* @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-teal.theme.css"); */
+" >> $HOME/.var/app/com.discordapp.Discord/config/BetterDiscord/data/stable/custom.css
