c6fe8ba01e2ebbb555a2887837290d4dfc8e0f28
Svenum <s.ziegler@holypenguin.net>
2023-07-17 20:58:14 +0200

    update discord script

--- scripts/prepare_discord.sh
+++ scripts/prepare_discord.sh
@@ -6,7 +6,24 @@ flatpak install --user com.discordapp.Discord -y
 echo "install betterdiscord ..."
 betterdiscordctl -i flatpak install
 
-echo "
+# Prepare theme
+echo "Do you want to use the light (l), dark (d) or none (n) theme?"
+read theme
+
+if [ $theme = "d" ]; then
+  echo "
+/* @import url("https://catppuccin.github.io/discord/dist/catppuccin-latte-teal.theme.css"); */
+@import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-teal.theme.css");
+" > $HOME/.var/app/com.discordapp.Discord/config/BetterDiscord/data/stable/custom.css
+elif [ $theme = "l" ]; then
+  echo "
+@import url("https://catppuccin.github.io/discord/dist/catppuccin-latte-teal.theme.css");
+/* @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-teal.theme.css"); */
+" > $HOME/.var/app/com.discordapp.Discord/config/BetterDiscord/data/stable/custom.css
+
+else
+  echo "
 /* @import url("https://catppuccin.github.io/discord/dist/catppuccin-latte-teal.theme.css"); */
 /* @import url("https://catppuccin.github.io/discord/dist/catppuccin-mocha-teal.theme.css"); */
 " >> $HOME/.var/app/com.discordapp.Discord/config/BetterDiscord/data/stable/custom.css
+fi
