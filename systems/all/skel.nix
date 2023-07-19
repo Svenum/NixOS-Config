44fa431cbddc0438999c3c3d4c0fe533f7fdf0a2
Svenum <s.ziegler@holypenguin.net>
2023-07-19 18:16:46 +0200

    fix bug

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -5,7 +5,6 @@ in
 {
   # Create Dir
   security.pam.makeHomeDir.skelDirectory = "/etc/skel";
-  environment.etc.skel.mode = "0600";
 
   # Copy items
   system.activationScripts = {
@@ -13,12 +12,11 @@ in
       text = ''
         for user in ${toString users}; do
           if [ ! -f /home/$user/.skel.lock ]; then
-            cp -rpf /etc/skel/. /home/$user/ && touch /home/$user/.skel.lock
-            chown -R $user:users /home/$user/
+            ${pkgs.rsync}/bin/rsync -a --chmod=744 --chown=$user:users /etc/skel/ /home/$user/ && touch /home/$user/.skel.lock
           fi
         done
         if [ ! -f /root/.skel.lock ]; then
-          cp -rpf /etc/skel/. /root/ && touch /root/.skel.lock
+          ${pkgs.rsync}/bin/rsync -a --chmod=744 --chown=root:root /etc/skel/ /root/ && touch /root/.skel.lock
         fi
       '';
     };
