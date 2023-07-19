5736edd16fe59107f9e6ff5ff0809fb073708d93
Svenum <s.ziegler@holypenguin.net>
2023-07-19 18:22:16 +0200

    add comment

--- systems/all/skel.nix
+++ systems/all/skel.nix
@@ -12,7 +12,7 @@ in
       text = ''
         for user in ${toString users}; do
           if [ ! -f /home/$user/.skel.lock ]; then
-            ${pkgs.rsync}/bin/rsync -a --chmod=744 --chown=$user:users -I /etc/skel/ /home/$user/ && touch /home/$user/.skel.lock
+            ${pkgs.rsync}/bin/rsync -a --chmod=744 --chown=$user:users -I /etc/skel/ /home/$user/ && touch /home/$user/.skel.lock || echo "Please Reboot that skel get applyed!"
           fi
         done
         if [ ! -f /root/.skel.lock ]; then
