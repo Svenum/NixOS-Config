c6d43ce4e40366f97fde40a7a18e60204c962bc1
Svenum <s.ziegler@holypenguin.net>
2023-07-23 01:36:00 +0200

    debug

--- systems/Ni/packages.nix
+++ systems/Ni/packages.nix
@@ -7,38 +7,7 @@
       rsync
       timeshift
       nvtop
-      (handbrake.overrideAttrs (_: { buildInputs = [
-        nv-codec-headers
-        a52dec
-        dav1d
-        ffmpeg-hb
-        fontconfig
-        freetype
-        fribidi
-        harfbuzz
-        jansson
-        lame
-        libass
-        libbluray
-        libdvdcss
-        libdvdnav
-        libdvdread
-        libiconv
-        libjpeg_turbo
-        libogg
-        libopus
-        libsamplerate
-        libtheora
-        libvorbis
-        libvpx
-        libxml2
-        speex
-        svt-av1
-        x264
-        x265
-        xz
-        zimg
-      ]; }))
+      (handbrake.overrideAttrs (_: { buildInputs = ++ ["nv-codec-headers"]; }))
     ];
   };
 
