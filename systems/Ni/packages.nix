af6a5dbf823511435cb6e11d01a87370faf8d369
Svenum <s.ziegler@holypenguin.net>
2023-07-23 01:36:37 +0200

    debug

--- systems/Ni/packages.nix
+++ systems/Ni/packages.nix
@@ -7,7 +7,7 @@
       rsync
       timeshift
       nvtop
-      (handbrake.overrideAttrs (_: { buildInputs = ++ ["nv-codec-headers"]; }))
+      (handbrake.overrideAttrs (_: { buildInputs = self.buildInputs ++ ["nv-codec-headers"]; }))
     ];
   };
 
