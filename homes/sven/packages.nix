024c89fd4c319f581efd6880ee5a8713e5157693
Svenum <s.ziegler@holypenguin.net>
2023-07-22 12:25:52 +0200

    Add nvenc

--- homes/sven/packages.nix
+++ homes/sven/packages.nix
@@ -3,5 +3,7 @@
 {
   home.packages = with pkgs; [
     caffeine-ng
+    handbrake
+    libdvdcss
   ];
 }
--- systems/Ni/nvidia.nix
+++ systems/Ni/nvidia.nix
@@ -2,7 +2,11 @@
 
 {
   hardware = {
-    opengl.enable = true;
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    }
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
@@ -10,4 +14,7 @@
     };
   };
   services.xserver.videoDrivers = [ "nvidia" ];
-}
+
+  environment.systemPackages = with pkgs; [
+    nv-codec-headers-11
+  ];
