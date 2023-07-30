6a5f1fe77f295477c70d158c2fb9334a754e3737
Svenum <s.ziegler@holypenguin.net>
2023-07-30 20:56:45 +0200

    remove ;

--- systems/Ni/nvidia.nix
+++ systems/Ni/nvidia.nix
@@ -9,7 +9,7 @@
       extraPackages = with pkgs; [
         vaapiVdpau
         libvdpau-va-gl
-        (nvidia_x11.override { enableGeforce1050Ti = true; };)
+        (nvidia_x11.override { enableGeforce1050Ti = true; })
       ];
     };
     nvidia = {
