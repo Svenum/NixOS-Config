983ee0e56816dfc4704ffc45d576f5f6e6e80734
Svenum <s.ziegler@holypenguin.net>
2023-07-31 13:13:22 +0200

    disable opensource module

--- systems/Ni/nvidia.nix
+++ systems/Ni/nvidia.nix
@@ -18,14 +18,14 @@
       modesetting.enable = true;
       #powerManagement.enable = true;
       #package = config.boot.kernelPackages.nvidiaPackages.beta;
-      open = true;
+      open = false;
     };
   };
   services.xserver.videoDrivers = [ "nvidia" ];
 
   # Uncomment if system does boot to terminal
-  boot.initrd.kernelModules = [ "nvidia" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  #boot.initrd.kernelModules = [ "nvidia" ];
+  #boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
   
   #environment.systemPackages = with pkgs; [
     #cudaPackages.cudatoolkit
