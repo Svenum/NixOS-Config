2e90f5f74699bd32445bb59a4e950a3e5b50ca6b
Svenum <s.ziegler@holypenguin.net>
2023-07-31 12:43:04 +0200

    add latest kernel

--- systems/Ni/kernel.nix
+++ systems/Ni/kernel.nix
@@ -1,10 +1,11 @@
-{ modulesPath, ... }:
+{ modulesPath, pkgs, ... }:
 
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" "sg" ];
