d2fa824aee1f40be450a470a2b495bfec1eb0f16
Svenum <s.ziegler@holypenguin.net>
2023-07-31 13:09:33 +0200

    change nvidia driver branch

--- systems/Ni/nvidia.nix
+++ systems/Ni/nvidia.nix
@@ -18,14 +18,15 @@
       modesetting.enable = true;
       #powerManagement.enable = true;
       #package = config.boot.kernelPackages.nvidiaPackages.beta;
-      package = config.boot.kernelPackages.nvidiaPackages.production;
       open = true;
     };
   };
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  boot.initrd.kernelModules = [ "nvidia" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  # Uncomment if system does boot to terminal
+  #boot.initrd.kernelModules = [ "nvidia" ];
+  #boot.extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+  
   #environment.systemPackages = with pkgs; [
     #cudaPackages.cudatoolkit
     #vaapiVdpau
