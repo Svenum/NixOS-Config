a2c6d14623fd15684c518519cdb8a18b1be88c0e
Svenum <s.ziegler@holypenguin.net>
2023-08-02 22:43:27 +0200

    Remove unnede lines

--- systems/San/packages.nix
+++ systems/San/packages.nix
@@ -14,10 +14,5 @@
   programs.gamemode.enable = true;
 
   # Tuxedo Control Center
-  nixpkgs.config.permittedInsecurePackages = [
-      "electron-13.6.9"
-      "nodejs-14.21.3"
-      "openssl-1.1.1u"
-  ];
   hardware.tuxedo-control-center.enable = true;
 }
