122ea69cf5cd5cd95dc951c53470915faf5c7ab2
Svenum <s.ziegler@holypenguin.net>
2023-08-02 23:17:58 +0200

    add catppuccin for tty

--- systems/Ni/kernel.nix
+++ systems/Ni/kernel.nix
@@ -8,6 +8,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];
   boot.kernelModules = [ "kvm-intel" "sg" ];
-  boot.kernelParams = ["mds=full,nosmt" "efi=runtime" "iommu=pt" "intel_iommu=on" ];
+  boot.kernelParams = ["mds=full,nosmt" "efi=runtime" "iommu=pt" "intel_iommu=on" "vt.default_red=30,243,166,249,137,245,148,186,88,243,166,249,137,245,148,166" "vt.default_grn=30,139,227,226,180,194,226,194,91,139,227,226,180,194,226,173" "vt.default_blu=46,168,161,175,250,231,213,222,112,168,161,175,250,231,213,200" ];
 
 }
