d73402064172c092929b5a034d388a33b11cf83a
Svenum <s.ziegler@holypenguin.net>
2023-08-03 21:31:02 +0200

    add amdgpu driver

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -2,18 +2,18 @@
 
 {
   hardware = {
-    #opengl = {
-    #  enable = true;
-    #  driSupport = true;
-    #  driSupport32Bit = true;
-    #  extraPackages = with pkgs; [
-    #    vaapiVdpau
-    #    libvdpau-va-gl
-    #  ];
-    #  extraPackages32 = with pkgs.pkgsi686Linux; [
-    #    vaapiVdpau
-    #  ];
-    #};
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vaapiVdpau
+      ];
+    };
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
@@ -28,5 +28,5 @@
       };
     };
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
 }
