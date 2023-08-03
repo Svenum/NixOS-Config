53d4e6dc4facdad70732f55d4edc832fea4140fa
Svenum <s.ziegler@holypenguin.net>
2023-08-03 21:34:13 +0200

    remove opengl

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -2,18 +2,18 @@
 
 {
   hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-      extraPackages32 = with pkgs.pkgsi686Linux; [
-        vaapiVdpau
-      ];
-    };
+    #opengl = {
+    #  enable = true;
+    #  driSupport = true;
+    #  driSupport32Bit = true;
+    #  extraPackages = with pkgs; [
+    #    vaapiVdpau
+    #    libvdpau-va-gl
+    #  ];
+    #  extraPackages32 = with pkgs.pkgsi686Linux; [
+    #    vaapiVdpau
+    #  ];
+    #};
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = true;
