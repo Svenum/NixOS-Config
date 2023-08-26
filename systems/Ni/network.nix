723ac4f930373c0a3dac99f791d7406d99944393
Svenum <s.ziegler@holypenguin.net>
2023-08-26 14:40:34 +0200

    allow wireguard

--- systems/Ni/network.nix
+++ systems/Ni/network.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   networking = {
@@ -6,4 +6,20 @@
     networkmanager.enable = true;
     useDHCP = lib.mkDefault true;
   };
+
+  # Allow Wireguard
+  networking.firewall = {
+   # if packets are still dropped, they will show up in dmesg
+   logReversePathDrops = true;
+   # wireguard trips rpfilter up
+   extraCommands = ''
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN
+     ip46tables -t mangle -I nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN
+   '';
+   extraStopCommands = ''
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --sport 51820 -j RETURN || true
+     ip46tables -t mangle -D nixos-fw-rpfilter -p udp -m udp --dport 51820 -j RETURN || true
+   '';
+  };
+}
 }
