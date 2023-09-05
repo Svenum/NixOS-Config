fedd06cd18bb5207cf1a1d4f72356408e6a8610c
Svenum <s.ziegler@holypenguin.net>
2023-09-05 15:26:46 +0200

    disable gpu on default

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -1,31 +1,38 @@
 { config, pkgs, lib, ... }:
 
 {
-  hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-      extraPackages32 = with pkgs.pkgsi686Linux; [
-        vaapiVdpau
-      ];
-    };
-    nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = true;
-      prime = {
-        offload = {
-          enableOffloadCmd = true;
+  hardware.nvidiaOptimus.disable;
+
+  specialisation = {
+  enable-nvidia.configuration = {
+    system.nixos.tags = [ "enable-nvidia" ];
+    hardware = {
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+        extraPackages = with pkgs; [
+          vaapiVdpau
+          libvdpau-va-gl
+        ];
+        extraPackages32 = with pkgs.pkgsi686Linux; [
+          vaapiVdpau
+        ];
+      };
+      nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = true;
+        prime = {
+          offload = {
+            enableOffloadCmd = true;
+          };
+          reverseSync.enable = lib.mkForce true;
+          amdgpuBusId = "PCI:5:0:0";
+          nvidiaBusId = "PCI:1:0:0";
         };
-        reverseSync.enable = lib.mkForce true;
-        amdgpuBusId = "PCI:5:0:0";
-        nvidiaBusId = "PCI:1:0:0";
       };
     };
-  };
-  services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+    services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+    }
+  }
 }
