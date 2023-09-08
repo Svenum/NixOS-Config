a4252521f1e59d9d22d7952ad142c5e164f4faab
Svenum <s.ziegler@holypenguin.net>
2023-09-09 00:46:26 +0200

    enable custom resize

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -4,10 +4,10 @@
   programs.tmux = {
     enable = true;
     newSession = true;
-    keyMode = "vi";
     historyLimit = 5000;
     resizeAmount = 15;
     clock24 = true;
+    customPaneNavigationAndResize = true; 
     terminal = "screen-256color";
     plugins = with pkgs.tmuxPlugins; [
       (pkgs.callPackage ../../custom-nixpkgs/catppuccin-tmux {})
