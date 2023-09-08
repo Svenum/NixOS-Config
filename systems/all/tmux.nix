fbf1b3c2ad2ca37d1744d7889459bc09c8e08463
Svenum <s.ziegler@holypenguin.net>
2023-09-09 00:52:25 +0200

    remove unneded form tmux conf

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -5,9 +5,9 @@
     enable = true;
     newSession = true;
     historyLimit = 5000;
-    resizeAmount = 15;
+    escapeTime = 200;
+    aggressiveResize = true;
     clock24 = true;
-    customPaneNavigationAndResize = true; 
     terminal = "screen-256color";
     plugins = with pkgs.tmuxPlugins; [
       (pkgs.callPackage ../../custom-nixpkgs/catppuccin-tmux {})
