2c293ec333fd16f332686079bd416fcdef271283
Svenum <s.ziegler@holypenguin.net>
2023-09-09 01:01:44 +0200

    add repeat time

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -6,11 +6,13 @@
     newSession = true;
     historyLimit = 5000;
     escapeTime = 200;
-    aggressiveResize = true;
     clock24 = true;
     terminal = "screen-256color";
     plugins = with pkgs.tmuxPlugins; [
       (pkgs.callPackage ../../custom-nixpkgs/catppuccin-tmux {})
     ];
+    extraConfig = ''
+      setw -g repeat-time 300
+    ''
   };
 }
