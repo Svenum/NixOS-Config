6a229aa2cb0e04d9985fa11246d7a71670d4a343
Svenum <s.ziegler@holypenguin.net>
2023-09-17 16:53:05 +0200

    add lp group

--- systems/all/user.nix
+++ systems/all/user.nix
@@ -5,13 +5,13 @@
   users.users.susven = {
     isNormalUser = true;
     description = "Susven";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "lp" ];
     useDefaultShell = true;
   };
   users.users.sven = {
     isNormalUser = true;
     description = "Sven";
-    extraGroups = [ "networkmanager" "libvirtd" "network" "video" "sys" "audio" "kvm" "optical" "scanner" "lpadmin" ];
+    extraGroups = [ "networkmanager" "libvirtd" "network" "video" "sys" "audio" "kvm" "optical" "scanner" "lpadmin" "lp" ];
     useDefaultShell = true;
   };
 }
