9f75ed67889415e2a2092972bafa98054d5b6fd8
Svenum <s.ziegler@holypenguin.net>
2023-09-17 17:48:14 +0200

    fix printer

--- systems/all/printer.nix
+++ systems/all/printer.nix
@@ -7,10 +7,15 @@
   services.avahi.nssmdns = true;
   services.avahi.openFirewall = true;
 
-  hardware.printers.ensurePrinters."Epson_ET-2720-Series" = {
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Epson_ET-2720-Series";
       deviceUri = "https://pr-epson.intra.holypenguin.net:631/ipp/print";
       description = "Epson ET-2720";
+      ppdOptions.PageSize = "A4";
     };
+    ensureDefaultPritner = "Epson_ET-2720-Series";
+  ];
 
   environment.systemPackages = with pkgs; [
     libinklevel
