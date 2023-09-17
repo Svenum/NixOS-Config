c22c1f9dbbe7aec745547493839a0d7780b30f5d
Svenum <s.ziegler@holypenguin.net>
2023-09-17 19:54:12 +0200

    update model

--- systems/all/printer.nix
+++ systems/all/printer.nix
@@ -16,7 +16,7 @@
         name = "Epson_ET-2720-Series";
         deviceUri = "https://pr-epson.intra.holypenguin.net:631/ipp/print";
         description = "Epson ET-2720";
-        model = "drv:///sample.drv/epson24.ppd";
+        model = "epson-inkjet-printer-escpr/Epson-ET-2720_Series-epson-escpr-en.ppd";
         ppdOptions.PageSize = "A4";
       }
     ];
