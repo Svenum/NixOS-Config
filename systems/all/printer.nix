850eda90dad2948f50a3d5cfd98e5a82c43b2a18
Svenum <s.ziegler@holypenguin.net>
2023-09-17 19:52:52 +0200

    import driver

--- systems/all/printer.nix
+++ systems/all/printer.nix
@@ -2,7 +2,10 @@
 
 {
   # Enable printer
-  services.printing.enable = true;
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.epson-escpr ];
+  };
   services.avahi.enable = true;
   services.avahi.nssmdns = true;
   services.avahi.openFirewall = true;
