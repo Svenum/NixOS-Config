c2f6e6da059492fa9609d13695ec104618f911f5
Svenum <s.ziegler@holypenguin.net>
2023-09-25 15:35:00 +0200

    enable nvidia

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -2,7 +2,35 @@
 
 {
   hardware.bumblebee.enable = true;
-  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vaapiVdpau
+      ];
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      prime = {
+        offload = {
+          enableOffloadCmd = true;
+        };
+        reverseSync.enable = true;
+        amdgpuBusId = "PCI:5:0:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
 
   specialisation = {
   enable-nvidia.configuration = {
@@ -34,7 +62,7 @@
         };
       };
     };
-    services.xserver.videoDrivers = lib.mkForce [ "nvidia" "amdgpu" ];
+    services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
     };
   };
 }
