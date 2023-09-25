fd4e716fa7d954dc948eebfc75f5144a3cd38b0a
Svenum <s.ziegler@holypenguin.net>
2023-09-25 19:40:55 +0200

    enable bumblebee

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -1,42 +1,45 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.xserver.videoDrivers = [ "nvidia" ];
-  boot.extraModprobeConfig = ''
-    blacklist vga16fb
-    blacklist nouveau
-    blacklist rivafb
-    blacklist nvidiafb
-    blacklist rivatv
-  '';
-
-  hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-      extraPackages = with pkgs; [
-        vaapiVdpau
-        libvdpau-va-gl
-      ];
-      extraPackages32 = with pkgs.pkgsi686Linux; [
-        vaapiVdpau
-      ];
-    };
-    nvidia = {
-      nvidiaSettings = true;
-      open = false;
-      modesetting.enable = true;
-      powerManagement.enable = true;
-      powerManagement.finegrained = true;
-      prime = {
-        offload = {
-          enableOffloadCmd = true;
-        };
-        reverseSync.enable = true;
-        amdgpuBusId = "PCI:5:0:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
+  hardware.bumblebee = {
+    enable = true;
   };
+  #services.xserver.videoDrivers = [ "nvidia" ];
+  #boot.extraModprobeConfig = ''
+  #  blacklist vga16fb
+  #  blacklist nouveau
+  #  blacklist rivafb
+  #  blacklist nvidiafb
+  #  blacklist rivatv
+  #'';
+
+  #hardware = {
+  #  opengl = {
+  #    enable = true;
+  #    driSupport = true;
+  #    driSupport32Bit = true;
+  #    extraPackages = with pkgs; [
+  #      vaapiVdpau
+  #      libvdpau-va-gl
+  #    ];
+  #    extraPackages32 = with pkgs.pkgsi686Linux; [
+  #      vaapiVdpau
+  #    ];
+  #  };
+  #  nvidia = {
+  #    nvidiaSettings = true;
+  #    open = false;
+  #    modesetting.enable = true;
+  #    powerManagement.enable = true;
+  #    powerManagement.finegrained = true;
+  #    prime = {
+  #      offload = {
+  #        enableOffloadCmd = true;
+  #      };
+  #      reverseSync.enable = true;
+  #      amdgpuBusId = "PCI:5:0:0";
+  #      nvidiaBusId = "PCI:1:0:0";
+  #    };
+  #  };
+  #};
 }
