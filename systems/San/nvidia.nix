7b1d7593e48fa4360bce21c19528ce1425d906ad
Svenum <s.ziegler@holypenguin.net>
2023-09-25 20:39:10 +0200

    add specification

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -1,45 +1,58 @@
 { config, pkgs, lib, ... }:
 
 {
-  hardware.bumblebee = {
-    enable = true;
-  };
-  #services.xserver.videoDrivers = [ "nvidia" ];
-  #boot.extraModprobeConfig = ''
-  #  blacklist vga16fb
-  #  blacklist nouveau
-  #  blacklist rivafb
-  #  blacklist nvidiafb
-  #  blacklist rivatv
-  #'';
+  services.xserver.videoDrivers = [ "nvidia" ];
+  boot.extraModprobeConfig = ''
+    blacklist vga16fb
+    blacklist nouveau
+    blacklist rivafb
+    blacklist nvidiafb
+    blacklist rivatv
+  '';
 
-  #hardware = {
-  #  opengl = {
-  #    enable = true;
-  #    driSupport = true;
-  #    driSupport32Bit = true;
-  #    extraPackages = with pkgs; [
-  #      vaapiVdpau
-  #      libvdpau-va-gl
-  #    ];
-  #    extraPackages32 = with pkgs.pkgsi686Linux; [
-  #      vaapiVdpau
-  #    ];
-  #  };
-  #  nvidia = {
-  #    nvidiaSettings = true;
-  #    open = false;
-  #    modesetting.enable = true;
-  #    powerManagement.enable = true;
-  #    powerManagement.finegrained = true;
-  #    prime = {
-  #      offload = {
-  #        enableOffloadCmd = true;
-  #      };
-  #      reverseSync.enable = true;
-  #      amdgpuBusId = "PCI:5:0:0";
-  #      nvidiaBusId = "PCI:1:0:0";
-  #    };
-  #  };
-  #};
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vaapiVdpau
+      ];
+    };
+    nvidia = {
+      nvidiaSettings = true;
+      open = false;
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      prime = {
+        offload = {
+          enableOffloadCmd = true;
+        };
+        reverseSync.enable = true;
+        amdgpuBusId = "PCI:5:0:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
+  };
+
+  specialisation = {
+    on-the-go.configuration = {
+      system.nixos.tags = [ "on-the-go" ];
+      services.xserver.videoDrivers = lib.mkForce [ "amdgpu" ];
+      boot.blacklistedKernelModules = [ "nouveau" "nvidiafb" "nvidia" ];
+      boot.extraModprobeConfig = ''
+        blacklist vga16fb
+        blacklist nouveau
+        blacklist rivafb
+        blacklist nvidiafb
+        blacklist rivatv
+        blacklist nvidia
+      '';
+    };
+  };
 }
