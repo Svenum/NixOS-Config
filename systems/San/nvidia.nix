3d6fac5d6883f8fe544a888384f83ed7872bf9f3
Svenum <s.ziegler@holypenguin.net>
2023-09-25 15:35:31 +0200

    remove specification

--- systems/San/nvidia.nix
+++ systems/San/nvidia.nix
@@ -31,38 +31,4 @@
       };
     };
   };
-
-  specialisation = {
-  enable-nvidia.configuration = {
-    system.nixos.tags = [ "enable-nvidia" ];
-    hardware = lib.mkForce {
-      nvidiaOptimus.disable = false;
-      opengl = {
-        enable = true;
-        driSupport = true;
-        driSupport32Bit = true;
-        extraPackages = with pkgs; [
-          vaapiVdpau
-          libvdpau-va-gl
-        ];
-        extraPackages32 = with pkgs.pkgsi686Linux; [
-          vaapiVdpau
-        ];
-      };
-      nvidia = {
-        modesetting.enable = true;
-        powerManagement.enable = true;
-        prime = {
-          offload = {
-            enableOffloadCmd = true;
-          };
-          reverseSync.enable = true;
-          amdgpuBusId = "PCI:5:0:0";
-          nvidiaBusId = "PCI:1:0:0";
-        };
-      };
-    };
-    services.xserver.videoDrivers = lib.mkForce [ "nvidia" ];
-    };
-  };
 }
