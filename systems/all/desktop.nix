b0ec8d41b3f8e2d49c9e2aabc17f5fcc08fe6331
Svenum <s.ziegler@holypenguin.net>
2023-09-26 18:54:42 +0200

    add compositor

--- systems/all/desktop.nix
+++ systems/all/desktop.nix
@@ -11,9 +11,9 @@
           DisplayServer = "wayland";
           GreeterEnvironment = "QT_WAYLAND_SHELL_INTEGRATION=layer-shell";
         };
-        #Wayland = {
-        #  CompositorCommand = "kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1";
-        #};
+        Wayland = {
+          CompositorCommand = "${pkgs.libsForQt5.kwin}/bin/kwin_wayland --drm --no-lockscreen --no-global-shortcuts --locale1";
+        };
       };
     };
     desktopManager.plasma5 = {
