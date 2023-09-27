0cdf79c89eb1e8c49b76c431975d07a84d440f25
Svenum <s.ziegler@holypenguin.net>
2023-09-27 10:35:25 +0200

    Update tmux

--- systems/San/tmux.nix
+++ systems/San/tmux.nix
@@ -5,13 +5,16 @@
     extraConfig = ''
       set -g mouse on
       set -g @catppuccin_flavour 'latte'
-      set -g @catppuccin_date_time "%d.%m.%Y %H:%M"
-      set -g @catppuccin_status_modules_right "application directory date_time "
-      set -g @catppuccin_powerline_icons_theme_enabled on
-      #set -g @catppuccin_window_left_separator ""
-      #set -g @catppuccin_window_right_separator ""
+      set -g @catppuccin_date_time_text "%d.%m.%Y %H:%M"
+      set -g @catppuccin_status_modules_right "application user date_time"
+      
+      set -g @catppuccin_window_left_separator "█"
+      set -g @catppuccin_window_right_separator ""
+      
       set -g @catppuccin_status_left_separator ""
-      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_status_right_separator "█"
+
+      set -g @catppuccin_window_current_text "#{pane_current_path}"
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
     '';
