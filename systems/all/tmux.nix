58a77b8bbb3988ab8e014dc0c70c739bc0dc6590
Svenum <s.ziegler@holypenguin.net>
2023-09-27 14:13:52 +0200

    update tmux config

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -24,8 +24,11 @@
       set -g @catppuccin_status_left_separator " "
       set -g @catppuccin_status_right_separator "█"
 
+      set -g @catppuccin_status_fill "all"
+
       set -g @catppuccin_window_current_text "#{pane_current_path}"
       set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_default_fill "all"
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
     '';
