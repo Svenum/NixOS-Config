8192c6c98f04b8991cab123995c7d29621d4ec59
Svenum <s.ziegler@holypenguin.net>
2023-09-27 10:21:13 +0200

    update tmux

--- systems/San/tmux.nix
+++ systems/San/tmux.nix
@@ -7,10 +7,10 @@
       set -g @catppuccin_flavour 'latte'
       set -g @catppuccin_date_time "%d.%m.%Y %H:%M"
       set -g @catppuccin_powerline_icons_theme_enabled on
-      set -g @catppuccin_l_left_separator ""
-      set -g @catppuccin_l_right_separator ""
-      set -g @catppuccin_r_left_separator ""
-      set -g @catppuccin_r_right_separator ""
+      set -g @catppuccin_window_left_separator ""
+      set -g @catppuccin_window_right_separator ""
+      set -g @catppuccin_status_left_separator ""
+      set -g @catppuccin_status_right_separator ""
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
     '';
