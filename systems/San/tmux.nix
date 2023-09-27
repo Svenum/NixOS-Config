86617c6ad3ed8b22a81295558e4f07c9e5e801ea
Svenum <s.ziegler@holypenguin.net>
2023-09-27 10:41:05 +0200

    update tmux

--- systems/San/tmux.nix
+++ systems/San/tmux.nix
@@ -6,15 +6,16 @@
       set -g mouse on
       set -g @catppuccin_flavour 'latte'
       set -g @catppuccin_date_time_text "%d.%m.%Y %H:%M"
-      set -g @catppuccin_status_modules_right "application user date_time"
+      set -g @catppuccin_status_modules "application user date_time"
       
       set -g @catppuccin_window_left_separator "█"
-      set -g @catppuccin_window_right_separator ""
+      set -g @catppuccin_window_right_separator " "
       
-      set -g @catppuccin_status_left_separator ""
+      set -g @catppuccin_status_left_separator " "
       set -g @catppuccin_status_right_separator "█"
 
       set -g @catppuccin_window_current_text "#{pane_current_path}"
+      set -g @catppuccin_window_status_enable "yes"
 
       run-shell ${pkgs.tmuxPlugins.catppuccin}/share/tmux-plugins/catppuccin/catppuccin.tmux
     '';
