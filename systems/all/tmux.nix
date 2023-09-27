eed98d7fa4962d5f6aeeba5bf5fe7377b4cd6efc
Svenum <s.ziegler@holypenguin.net>
2023-09-27 14:33:26 +0200

    update tmux config

--- systems/all/tmux.nix
+++ systems/all/tmux.nix
@@ -20,12 +20,14 @@
       
       set -g @catppuccin_window_right_separator " "
       set -g @catppuccin_window_left_separator ""
-      set -g @catppuccin_window_middle_separator "|"
+      set -g @catppuccin_window_left_separator_inverse "yes"
       
       set -g @catppuccin_status_left_separator " "
-      set -g @catppuccin_status_right_separator " "
+      set -g @catppuccin_status_right_separator ""
+      set -g @catppuccin_status_right_separator_inverse "yes"
 
       set -g @catppuccin_status_fill "all"
+      set -g @catppuccin_status_connect_separator "no"
 
       set -g @catppuccin_window_current_text "#{pane_current_path}"
       set -g @catppuccin_window_number_position "right"
