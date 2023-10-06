1692372b05a213bf9ecc9460dcd8956862a96a42
Svenum <s.ziegler@holypenguin.net>
2023-10-06 12:03:26 +0200

    update fs

--- systems/srv-nixostest/fs.nix
+++ systems/srv-nixostest/fs.nix
@@ -2,12 +2,14 @@
 
 {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/9ad867b6-2386-4283-87be-6108191a82f8";
+    { 
+      lable = "NixOS"
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/8405-146C";
+    { 
+      label = "EFI";
       fsType = "vfat";
     };
 
