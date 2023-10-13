3e6e733de05814da9106fed3de48b11809b400a1
Svenum <s.ziegler@holypenguin.net>
2023-10-14 01:19:31 +0200

    update tlp

--- systems/San/tlp.nix
+++ systems/San/tlp.nix
@@ -25,7 +25,7 @@
       RESTORE_DEVICE_STATE_ON_STARTUP = 0;
       DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth wifi";
       DEVICES_TO_ENABLE_ON_STARTUP = "wwan";
-      DEVICES_TO_ENABLE_ON_AC = "wifi wwan";
+      DEVICES_TO_ENABLE_ON_AC = "wifi";
       DEVICES_TO_DISABLE_ON_BAT_NOT_IN_USE = "wifi bluetooth";
       RUNTIME_PM_ON_AC = "on";
       RUNTIME_PM_ON_BAT = "auto";
