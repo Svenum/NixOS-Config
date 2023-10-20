cd9408d5567a6394012c87d0b2f3e9d578518569
Svenum <s.ziegler@holypenguin.net>
2023-10-20 13:59:08 +0200

    debug

--- systems/all/flatpak-fix.nix
+++ systems/all/flatpak-fix.nix
@@ -22,5 +22,6 @@
     # Create an FHS mount to support flatpak host icons/fonts
     "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
     "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
+    "/usr/share/themes" = mkRoSymBind (aggregatedThemes + "/share/themes");
   };
 }
