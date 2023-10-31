48ed1a5c343c53032f445bb5e36f43c79372bf3c
Svenum <s.ziegler@holypenguin.net>
2023-11-01 00:54:26 +0100

    debug

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -5,5 +5,5 @@
   users.users.root.hashedPassword = "!";
 
   # Config other users
-  lib.forEach = userList: user: lib.import ./userConfig.nix;
+  lib.lists.forEach = userList: user: lib.import ./userConfig.nix;
 }
