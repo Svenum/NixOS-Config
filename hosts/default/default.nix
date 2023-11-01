6974a45b8f97f85b0722c7ea711923dd1d505b87
Svenum <s.ziegler@holypenguin.net>
2023-11-01 14:10:22 +0100

    add packages back

--- hosts/default/default.nix
+++ hosts/default/default.nix
@@ -5,17 +5,17 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     ../../modules/user
     ./bootloader.nix
-    #./packages.nix
-    #./plymouth.nix
+    ./packages.nix
+    ./plymouth.nix
     ./desktop.nix
     ./language.nix
-    #./nvim.nix
-    #./tmux.nix
-    #./shell.nix
-    #./skel.nix
+    ./nvim.nix
+    ./tmux.nix
+    ./shell.nix
+    ./skel.nix
     ./kernel.nix
     ./network.nix
-    #./printer.nix
+    ./printer.nix
   ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config = {
