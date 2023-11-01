2f7c05d7e68de4c767b60327f1d143ab3f80bf6a
Svenum <s.ziegler@holypenguin.net>
2023-11-01 16:05:04 +0100

    update flatpak config

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -1,5 +1,17 @@
-{ users, lib, pkgs, config, home-manager, ... }:
+{ userAttrs, lib, pkgs, config, home-manager, ... }:
 
+let
+  mkUserConfig = name: user: {
+    home.activation = {
+      configureFlatpak = ''
+        ${pkgs.flatpak}/bin/flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+        ${pkgs.flatpak}/bin/flatpak remote-add --user --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+        ${pkgs.flatpak}/bin/flatpak override --user --filesystem=xdg-config/gtk-3.0:ro --filesystem=xdg-config/gtkrc-2.0:ro --filesystem=xdg-config/gtk-4.0:ro --filesystem=xdg-config/gtkrc:ro --filesystem=~/.themes:ro
+        ${pkgs.flatpak}/bin/flatpak override --user --device=dri --filesystem=~/Games:rw com.valvesoftware.Steam
+      '';
+    };
+  };
+in
 {
   # Install and enable flatpak
   services.flatpak.enable = true;
@@ -26,7 +38,8 @@
   };
 
   # Add repo and add overrides for guiUsers
-  lib.forEach = users: user: lib.import ./userConfig.nix;
+  imports = [ home-manager.nixosModules.home-manager ];
+  home-manager.users = lib.mapAttrs mkUserConfig userAttrs;
 }
 
 
