9f340c1e9ce742ccd99dcf05afa6602e46d4494d
Svenum <s.ziegler@holypenguin.net>
2023-11-01 09:57:45 +0100

    debug

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,9 +1,25 @@
-{ userList, lib, ... }:
+{ userList, lib, pkgs, ... }:
 
-rec {
+let
+  createUserConfig = {user}: { 
+    # Create user
+    user.name = {
+      isNormalUser = true;
+      description = user.name;
+      # Add user to default groups
+      extraGroups = [ "networkmanager" "libvirtd" "network" "video" "sys" "audio" "kvm" "optical" "scanner" "lp" (lib.mkIf (user.sudo ? false) [ "wheel" ])];
+      useDefaultShell = true;
+    };
+  };
+in
+{
   # Disable root
-  users.users.root.hashedPassword = "!";
+  #users.users.root.hashedPassword = "!";
+  #users.defaultUserShell = pkgs.zsh;
+
+  # Create user
+  lib.lists.forEach = userList: user: createUserConfig user;
 
   # Config other users
-  lib.lists.forEach = userList: user: import ./userConfig.nix;
+  #lib.lists.forEach = userList: user: import ./userConfig.nix;
 }
