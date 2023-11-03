f5be33e6c360517bfafd75672ed0adbdde60e711
Svenum <s.ziegler@holypenguin.net>
2023-11-03 20:33:51 +0100

    add nvim module

--- hosts/default/default.nix
+++ hosts/default/default.nix
@@ -5,10 +5,10 @@
     (modulesPath + "/installer/scan/not-detected.nix")
     ../../modules/user
     ../../modules/boot
+    ../../modules/nvim
     ./packages.nix
     ./desktop.nix
     ./language.nix
-    ./nvim.nix
     ./tmux.nix
     ./shell.nix
     ./skel.nix
