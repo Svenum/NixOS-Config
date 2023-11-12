373e2e2e32b102dd6bcb15bf676f7ae4d121c191
Svenum <s.ziegler@holypenguin.net>
2023-11-12 20:39:19 +0100

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -119,7 +119,7 @@
     
     tuxedo-nixos = {
       url = "github:Svenum/tuxedo-nixos";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     };
 
     home-manager = {
