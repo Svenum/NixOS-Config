1c9f6e4d7327098f9d545046590b7e06c99d9199
Svenum <s.ziegler@holypenguin.net>
2023-12-02 18:06:55 +0100

    fix kvm

--- modules/kvm/default.nix
+++ modules/kvm/default.nix
@@ -2,8 +2,12 @@
 
 {
   virtualisation.libvirtd.enable = true;
-  programs.dconf.enable = true;
-  environment.systemPackages = with pkgs; [
-    virt-manager
-  ];
+  programs.virt-manager.enable = true;
+  virtualisation.spiceUSBRedirection.enable = true;
+  dconf.settings = {
+    "org/virt-manager/virt-manager/connections" = {
+      autoconnect = ["qemu:///system"];
+      uris = ["qemu:///system"];
+    };
+  };
 }
