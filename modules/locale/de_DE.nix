4c6e42484a8ecee887f6542b4e9c1126b9fc2cbf
Svenum <s.ziegler@holypenguin.net>
2023-12-10 14:32:28 +0100

    fix locale

--- modules/locale/de_DE.nix
+++ modules/locale/de_DE.nix
@@ -3,16 +3,5 @@
 {
   i18n = {
     defaultLocale = "de_DE.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "de_DE.UTF-8";
-      LC_IDENTIFICATION = "de_DE.UTF-8";
-      LC_MEASUREMENT = "de_DE.UTF-8";
-      LC_MONETRAY = "de_DE.UTF-8";
-      LC_NAME = "de_DE.UTF-8";
-      LC_NUMERIC = "de_DE.UTF-8";
-      LC_PAPER = "de_DE.UTF-8";
-      LC_TELEPHONE = "de_DE.UTF-8";
-      LC_TIME = "de_DE.UTF-8";
-    };
   };
 }
