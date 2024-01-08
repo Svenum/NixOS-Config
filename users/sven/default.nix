b79f42f5981044dff4319616fa914f070118c559
Svenum <s.ziegler@holypenguin.net>
2024-01-08 18:24:14 +0100

    add first shortcuts

--- users/sven/default.nix
+++ users/sven/default.nix
@@ -14,5 +14,21 @@ in
 
   programs.plasma = {
     enable = true;
+    shortcuts = {
+      kwin = {
+        # Switch Desktop
+        "Switch One Desktop Down" = "Ctrl+Alt+Down";
+        "Switch One Desktop Up" = "Ctrl+Alt+Up";
+        "Switch One Desktop to the Left" = "Ctrl+Alt+Left";
+        "Switch One Desktop to the Right" = "Ctrl+Alt+Right";
+        # Move Window to diffrent Desktop
+        "Window One Desktop Down" = "Meta+Ctrl+Alt+Down";
+        "Window One Desktop Up" = "Meta+Ctrl+Alt+Up";
+        "Window One Desktop to the Left" = "Meta+Ctrl+Alt+Left";
+        "Window One Desktop to the Right" = "Meta+Ctrl+Alt+Right";
+
+
+      };
+    };
   };
 }
