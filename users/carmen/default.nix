c4ae36cb3a8445dffe20ebb1b32a4f175af38dca
Svenum <s.ziegler@holypenguin.net>
2024-01-20 15:55:21 +0100

    add theme

---
+++ users/carmen/default.nix
@@ -0,0 +1,13 @@
+{ home, pkgs, ... }:
+
+let
+  tetris = pkgs.callPackage ../../custom-nixpkgs/tetris {};
+in
+{
+  # Add extgra packages
+  home.packages = with pkgs; [
+    caffeine-ng
+    tetris
+    skanpage
+  ];
+}
