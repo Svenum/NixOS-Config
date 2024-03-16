d24171390eddb93d1f3900246196df54d79aea0f
Svenum <s.ziegler@holypenguin.net>
2024-03-16 12:23:01 +0100

    disable printer

--- hosts/Ni/settings.nix
+++ hosts/Ni/settings.nix
@@ -42,23 +42,23 @@
   };
 
   # Printer Settings
-  enableScanner = true;
-  printerAttrs =[
-    {
-      name = "Epson_ET-2720-Series";
-      deviceUri = "https://pr-epson.intra.holypenguin.net:631/ipp/print";
-      model = "epson-inkjet-printer-escpr/Epson-ET-2720_Series-epson-escpr-en.ppd";
-      description = "Epson ET-2720";
-      isDefault = true;
-    }
-    {
-      name = "HP_Officejet_5740-Series";
-      deviceUri = "https://pr-hp.intra.holypenguin.net/ipp/printers";
-      model = "HP/hp-officejet_5740_series.ppd.gz";
-      description = "HP Officejet 5740";
-      isDefault = false;
-    }
-  ]; 
+  #enableScanner = true;
+  #printerAttrs =[
+  #  {
+  #    name = "Epson_ET-2720-Series";
+  #    deviceUri = "https://pr-epson.intra.holypenguin.net:631/ipp/print";
+  #    model = "epson-inkjet-printer-escpr/Epson-ET-2720_Series-epson-escpr-en.ppd";
+  #    description = "Epson ET-2720";
+  #    isDefault = true;
+  #  }
+  #  {
+  #    name = "HP_Officejet_5740-Series";
+  #    deviceUri = "https://pr-hp.intra.holypenguin.net/ipp/printers";
+  #    model = "HP/hp-officejet_5740_series.ppd.gz";
+  #    description = "HP Officejet 5740";
+  #    isDefault = false;
+  #  }
+  #]; 
 
   # Shell Settings
   shell = "zsh";
