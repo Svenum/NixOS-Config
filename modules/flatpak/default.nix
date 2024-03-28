d2aa1c6636cf7c7f28b8fc8826a1b5512479468f
Svenum <s.ziegler@holypenguin.net>
2024-03-28 17:58:12 +0100

    remove flatpak workaround

--- modules/flatpak/default.nix
+++ modules/flatpak/default.nix
@@ -33,23 +33,23 @@ in
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   # Fix Icons and fonts for Flatpak
-  system.fsPackages = [ pkgs.bindfs ];
-  fileSystems = let
-    mkRoSymBind = path: {
-      device = path;
-      fsType = "fuse.bindfs";
-      options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
-    };
-    aggregatedFonts = pkgs.buildEnv {
-      name = "system-fonts";
-      paths = config.fonts.packages;
-      pathsToLink = [ "/share/fonts" ];
-    };
-  in {
-    # Create an FHS mount to support flatpak host icons/fonts
-    "/usr/share/icons" = mkRoSymBind "${config.system.path}/share/icons";
-    "/usr/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
-  };
+  #system.fsPackages = [ pkgs.bindfs ];
+  #fileSystems = let
+  #  mkRoSymBind = path: {
+  #    device = path;
+  #    fsType = "fuse.bindfs";
+  #    options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+  #  };
+  #  aggregatedFonts = pkgs.buildEnv {
+  #    name = "system-fonts";
+  #    paths = config.fonts.packages;
+  #    pathsToLink = [ "/share/fonts" ];
+  #  };
+  #in {
+  #  # Create an FHS mount to support flatpak host icons/fonts
+  #  "/usr/share/icons" = mkRoSymBind "${config.system.path}/share/icons";
+  #  "/usr/share/fonts" = mkRoSymBind "${aggregatedFonts}/share/fonts";
+  #};
 
   # Add repo and add overrides for guiUsers
   imports = [ home-manager.nixosModules.home-manager ];
