f7b7cfac248383896b74ed87afccac596e99f748
Svenum <s.ziegler@holypenguin.net>
2024-03-29 20:03:23 +0100

    remove gpu bind

--- modules/kvm/default.nix
+++ modules/kvm/default.nix
@@ -1,35 +1,35 @@
 { lib, pkgs, settings, config, ... }:
 
 let
-  script = ''
-    #!${pkgs.bash}
-    list="${lib.strings.concatMapStrings (x: " " + x) settings.pciPassthrough.isolatedDevices}"
+  #script = ''
+  #  #!${pkgs.bash}
+  #  list="${lib.strings.concatMapStrings (x: " " + x) settings.pciPassthrough.isolatedDevices}"
 
-    for item in $list; do                                                           
-      driver_in_use=$(lspci -d $item -k | grep "Kernel driver in use:" | awk '{print $NF}')
-      kernel_module=$(lspci -d $item -k | grep "Kernel modules:" | awk '{print $NF}')
-      pci_slot=$(lspci -d $item -D | awk '{print $1}')
+  #  for item in $list; do                                                           
+  #    driver_in_use=$(lspci -d $item -k | grep "Kernel driver in use:" | awk '{print $NF}')
+  #    kernel_module=$(lspci -d $item -k | grep "Kernel modules:" | awk '{print $NF}')
+  #    pci_slot=$(lspci -d $item -D | awk '{print $1}')
 
-      if [[ $driver_in_use == $kernel_module ]]; then
-        echo "Bind $pci_slot to vfio-pci!"
-        echo $pci_slot > /sys/bus/pci/drivers/$kernel_module/unbind
-        echo $pci_slot > /sys/bus/pci/drivers/vfio-pci/bind
-      elif [[ $driver_in_use == "vifo-pci" ]]; then
-        echo "Bind $pci_slot to $kernel_module!"
-        echo $pci_slot > /sys/bus/pci/drivers/vfio-pci/unbind
-        echo $pci_slot > /sys/bus/pci/drivers/$kernel_module/bind
-      elif [[ $driver_in_use == "" ]]; then
-        echo "Bind $pci_slot to $kernel_module"
-        echo $pci_slot > /sys/bus/pci/drivers/$kernel_module/bind
-      else
-        echo "Something is wront with $pci_slot. It uses not $kernel_module or vfio-pci!"
-        exit 1
-      fi
-    done
-  '';
-  toggle_vfio = lib.mkIf settings.pciPassthrough.enable (
-    pkgs.writeShellScriptBin "toggle_vfio" script
-  );
+  #    if [[ $driver_in_use == $kernel_module ]]; then
+  #      echo "Bind $pci_slot to vfio-pci!"
+  #      echo $pci_slot > /sys/bus/pci/drivers/$kernel_module/unbind
+  #      echo $pci_slot > /sys/bus/pci/drivers/vfio-pci/bind
+  #    elif [[ $driver_in_use == "vifo-pci" ]]; then
+  #      echo "Bind $pci_slot to $kernel_module!"
+  #      echo $pci_slot > /sys/bus/pci/drivers/vfio-pci/unbind
+  #      echo $pci_slot > /sys/bus/pci/drivers/$kernel_module/bind
+  #    elif [[ $driver_in_use == "" ]]; then
+  #      echo "Bind $pci_slot to $kernel_module"
+  #      echo $pci_slot > /sys/bus/pci/drivers/$kernel_module/bind
+  #    else
+  #      echo "Something is wront with $pci_slot. It uses not $kernel_module or vfio-pci!"
+  #      exit 1
+  #    fi
+  #  done
+  #'';
+  #toggle_vfio = lib.mkIf settings.pciPassthrough.enable (
+  #  pkgs.writeShellScriptBin "toggle_vfio" script
+  #);
 in
 {
   virtualisation = {
@@ -40,19 +40,19 @@ in
 
   environment.systemPackages = with pkgs; [
     libguestfs
-    toggle_vfio
+    #toggle_vfio
   ];
 
-  security.wrappers."toggle_vfio" = {
-    owner = "root";
-    group = "root";
-    setuid = true;
-    source = "${toggle_vfio}/bin/toggle_vfio";
-    permissions = "g+rx,o+rx";
-  };
+  #security.wrappers."toggle_vfio" = {
+  #  owner = "root";
+  #  group = "root";
+  #  setuid = true;
+  #  source = toggle_vfio + /bin/toggle_vfio;
+  #  permissions = "g+rx,o+rx";
+  #};
 
   boot.extraModprobeConfig = lib.mkIf settings.pciPassthrough.enable ''
-    options vfio_pci ids=${lib.strings.concatMapStrings (x: "," + x) settings.pciPassthrough.isolatedDevices}
+    #options vfio_pci ids=${lib.strings.concatMapStrings (x: "," + x) settings.pciPassthrough.isolatedDevices}
     options vfio_iommu_type1 allow_unsafe_interrupts=1
     options vfio_pci disable_vga=1
     options vfio_pci enable_sriov=1
--- users/sven/plasma.nix
+++ users/sven/plasma.nix
@@ -112,7 +112,7 @@ in
                 showName = "false";
                 showFullName = "false";
                 showLockScreen = "false"; 
-                showLogOut = "false";
+                showLogOut = "true";
                 showHibernate = "true";
                 showSuspend = "true";
                 showNewSession = "false";
