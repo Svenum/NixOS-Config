255fe8d2e49001c88187bf536879d35d6b49ae5b
Svenum <s.ziegler@holypenguin.net>
2024-03-31 01:07:53 +0100

    fix

--- users/sven/kvm.nix
+++ users/sven/kvm.nix
@@ -12,12 +12,24 @@ in
     pools = [
       {
         definition = nixvirt.lib.pool.writeXML {
+          name = "default";
+          uuid = "a72e2000-ab5e-4c2d-a571-daf611fc77a4";
+          type = "dir";
+          target = {
+            path = "/home/sven/.local/share/libvirt/images";
+          };
+          active = true;
+        };
+      }
+      {
+        definition = nixvirt.lib.pool.writeXML {
           name = "isos";
           uuid = "5217ddb8-29c2-4a4d-b976-73b9dde59e43";
           type = "dir";
           target = {
             path = "/home/sven/.local/share/libvirt/isos";
           };
+          active = true;
         };
       }
       {
@@ -28,53 +40,48 @@ in
           target = {
             path = "/home/sven/.local/share/libvirt/qemu";
           };
+          active = true;
         };
       }
     ];
     # Add windows Domain
-    #domains = [
-    #  (
-    #    lib.mkIf (systemConfig.networking.hostName == "Shi")
-    #    {
-    #      definition = nixvirt.lib.domain.writeXML (nixvirt.lib.domain.templates.windows
-    #        {
-    #          name = "win10";
-    #          uuid = "a9329510-9185-4849-a4ed-0b52aa2f4d47";
-    #          memory = { count = 24; unit = "GiB"; };
-    #          storage_vol = { pool = "default"; volume = "win10.qcow2"; };
-    #          nvram_path = nvramDefaultPath + /win10.nvram;
-    #          virtio_net = true;
-    #          virtio_drive = true;
-    #          install_virtio = true;
-    #        }
-    #      );
-    #    }
+    domains = [
+        {
+          definition = lib.mkIf (systemConfig.networking.hostName == "Shi") nixvirt.lib.domain.writeXML (nixvirt.lib.domain.templates.windows
+            {
+              name = "win10";
+              uuid = "a9329510-9185-4849-a4ed-0b52aa2f4d47";
+              memory = { count = 24; unit = "GiB"; };
+              storage_vol = { pool = "default"; volume = "win10.qcow2"; };
+              nvram_path = nvramDefaultPath + /win10.nvram;
+              virtio_net = true;
+              virtio_drive = true;
+              install_virtio = true;
+            }
+          );
 
 
 
-
-
-
-    #    #{
-    #    #  definition = nixvirt.lib.pool.writeXML {
-    #    #    vcpu.count = 12;
-    #    #    os = {
-    #    #      type = "pc-q35-8.2"
-    #    #      arch = "x86_64";
-    #    #      machine = "qt5"
-    #    #      boot = [ { dev = "hd"; } ];
-    #    #      loader = {
-    #    #        readonly = true;
-    #    #        type = "pflash";
-    #    #        path = "${nixvirt.lib.nixpkgs-ovmf.OVMFFull.fd}/FV/OVMF_CODE.ms.fd";
-    #    #      };
-    #    #      nvram = {
-    #    #        
-    #    #      };
-    #    #    };
-    #    #  };
-    #    #}
-    #  )
-    #];
+        #{
+        #  definition = nixvirt.lib.pool.writeXML {
+        #    vcpu.count = 12;
+        #    os = {
+        #      type = "pc-q35-8.2"
+        #      arch = "x86_64";
+        #      machine = "qt5"
+        #      boot = [ { dev = "hd"; } ];
+        #      loader = {
+        #        readonly = true;
+        #        type = "pflash";
+        #        path = "${nixvirt.lib.nixpkgs-ovmf.OVMFFull.fd}/FV/OVMF_CODE.ms.fd";
+        #      };
+        #      nvram = {
+        #        
+        #      };
+        #    };
+        #  };
+        #}
+      )
+    ];
   };
 }
