bb8abea422a441892baa1185d7b9fa3820cb3534
Svenum <s.ziegler@holypenguin.net>
2024-04-03 15:45:55 +0200

    disable sddm fprintd

--- hosts/Shi/default.nix
+++ hosts/Shi/default.nix
@@ -40,14 +40,6 @@
   services.fprintd.enable = true;
   security.pam.services.sudo.fprintAuth = false;
   security.pam.services.login.fprintAuth = false;
-  security.pam.services.sddm.text = lib.mkForce ''
-    auth      [success=1 new_authtok_reqd=1 default=ignore] pam_unix.so try_first_pass likeauth nullok
-    auth      sufficient    ${config.services.fprintd.package}/lib/security/pam_fprintd.so
-    auth      substack      login
-    account   include       login
-    password  substack      login
-    session   include       login
-  '';
 
   # Fix Wlan after suspend or Hibernate
   environment.etc."systemd/system-sleep/fix-wifi.sh".source =
