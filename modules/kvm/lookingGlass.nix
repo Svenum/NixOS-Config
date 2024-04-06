584f847959a5c0aede912f2a38b4a1e66d6c7767
Svenum <s.ziegler@holypenguin.net>
2024-04-07 00:24:22 +0200

    remove overlay

--- modules/kvm/lookingGlass.nix
+++ modules/kvm/lookingGlass.nix
@@ -12,19 +12,6 @@ let
   };
 in
 {
-  # disable terminal in looking-glass.desktop
-  nixpkgs.overlays = [
-    (
-      final: prev: {
-        looking-glass-client = prev.looking-glass-client.overrideAttrs ( old: {
-          postInstall = old.postInstall + ''
-            substituteInPlace $out/share/applications/looking-glass-client.desktop --replace 'Terminal=true' 'Terminal=false' 
-          '';
-        });
-      }
-    )
-  ];
-
   # install looking-glass-client
   environment.systemPackages = with pkgs; [
     (lib.mkIf settings.pciPassthrough.enable or false looking-glass-client)
