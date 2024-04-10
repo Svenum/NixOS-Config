d2997d9a930413d5e1832d8505c20a90984693ed
Svenum <s.ziegler@holypenguin.net>
2024-04-10 21:40:51 +0200

    debug

--- hosts/Shi/kvm.nix
+++ hosts/Shi/kvm.nix
@@ -2,10 +2,16 @@
 
 let
   toggle_gpu = pkgs.writeShellScriptBin "toggle_gpu" ''
+    echo >> /tmp/MESSAGE.txt
+    echo $(date) >> /tmp/MESSAGE.txt
+    echo $@ >> /tmp/MESSAGE.txt
     if [[ $1 == "Windows GPU Nix" ]]; then
+      echo "DOMAIN FOUND" >> /tmp/MESSAGE.txt
       if [[ $2 == "prepare" ]]; then
+        echo "PREPARE" >> /tmp/MESSAGE.txt
         /run/current-system/sw/bin/toggle_gpu vfio
       elif [[ $2 == "stopped" ]]; then
+        echo "STOPPING" >> /tmp/MESSAGE.txt
         /run/current-system/sw/bin/toggle_gpu amd
       fi
     fi
