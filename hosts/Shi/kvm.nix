e9e77ecad346869dc08df5cdcec9629b5eddd1db
Svenum <s.ziegler@holypenguin.net>
2024-04-10 21:22:35 +0200

    debug

--- hosts/Shi/kvm.nix
+++ hosts/Shi/kvm.nix
@@ -8,11 +8,12 @@ let
 
   load_vfio = pkgs.writeShellScriptBin "load_vfio" ''
     #/run/current-system/sw/bin/toggle_gpu vfio
+    echo $@ > /tmp/MESSAGE.txt
   '';
 in
 {
   virtualisation.libvirtd.hooks.daemon = {
-    "load_vfio" = load_vfio;
+    "load_vfio" = load_vfio/bin/load_vfio;
     #"Windows GPU Nix/stopped/end/load_amd" = load_amd;
   };
 }
