74d48abc3585bc02e9726dde5f9f79965e5f2f0f
Svenum <s.ziegler@holypenguin.net>
2024-04-10 18:23:17 +0200

    fix

--- hosts/Shi/kvm.nix
+++ hosts/Shi/kvm.nix
@@ -1,8 +1,6 @@
-{ ... }:
-
 {
   virtualisation.libvirtd.hooks.qemu = {
-    "toggle_gpu" = "/run/current-system/sw/bin/toggle_gpu vfio";
+    toggle_gpu = "/run/current-system/sw/bin/toggle_gpu vfio";
     #toggle_gpu" = "/run/current-system/sw/bin/toggle_gpu amd";
   };
 }
