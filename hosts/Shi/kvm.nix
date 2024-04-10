0bb8965fbbdef7bc3034bbc1ef49d583af2b6881
Svenum <s.ziegler@holypenguin.net>
2024-04-10 21:21:05 +0200

    debug

--- hosts/Shi/kvm.nix
+++ hosts/Shi/kvm.nix
@@ -2,16 +2,17 @@
 
 let
   load_amd = pkgs.writeShellScriptBin "load_amd" ''
-    /run/current-system/sw/bin/toggle_gpu amd
+    #/run/current-system/sw/bin/toggle_gpu amd
+    echo $@ > /tmp/MESSAGE.txt
   '';
 
   load_vfio = pkgs.writeShellScriptBin "load_vfio" ''
-    /run/current-system/sw/bin/toggle_gpu vfio
+    #/run/current-system/sw/bin/toggle_gpu vfio
   '';
 in
 {
   virtualisation.libvirtd.hooks.daemon = {
-    "Windows GPU Nix/prepare/begin/load_vfio" = load_vfio;
-    "Windows GPU Nix/stopped/end/load_amd" = load_amd;
+    "load_vfio" = load_vfio;
+    #"Windows GPU Nix/stopped/end/load_amd" = load_amd;
   };
 }
