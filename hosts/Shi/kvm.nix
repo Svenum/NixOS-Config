a57903f4ed584c1fcd60e73636ded80e346771d9
Svenum <s.ziegler@holypenguin.net>
2024-04-11 07:33:21 +0200

    add sleep

--- hosts/Shi/kvm.nix
+++ hosts/Shi/kvm.nix
@@ -2,18 +2,12 @@
 
 let
   toggle_gpu = pkgs.writeShellScriptBin "toggle_gpu" ''
-    echo >> /tmp/MESSAGE.txt
-    echo $(date) >> /tmp/MESSAGE.txt
-    echo $(id -u) >> /tmp/MESSAGE.txt
-    echo $@ >> /tmp/MESSAGE.txt
     if [[ $1 == "Windows GPU Nix" ]]; then
-      echo "DOMAIN FOUND" >> /tmp/MESSAGE.txt
       if [[ $2 == "prepare" ]]; then
-        echo "PREPARE" >> /tmp/MESSAGE.txt
-        /run/current-system/sw/bin/toggle_gpu vfio &>> /tmp/MESSAGE.txt
+        /run/current-system/sw/bin/toggle_gpu vfio
       elif [[ $2 == "stopped" ]]; then
-        echo "STOPPING" >> /tmp/MESSAGE.txt
-        /run/current-system/sw/bin/toggle_gpu amd &>> /tmp/MESSAGE.txt
+        ${pkgs.busybox}/bin/sleep 10
+        /run/current-system/sw/bin/toggle_gpu amd
       fi
     fi
   '';
