f62b20ff166bf8f3cc81da443df58764f19339d0
Svenum <s.ziegler@holypenguin.net>
2024-04-17 17:45:55 +0200

    add module

--- hosts/Yon/default.nix
+++ hosts/Yon/default.nix
@@ -24,6 +24,7 @@
     ../../modules/amdgpu
     ../../modules/controller
     ../../modules/powerManagement
+    ../../modules/secureBoot
 
     # Import system flakes
     nixVirt.nixosModules.default
@@ -34,16 +35,6 @@
     # Import specific modules
     ./kvm.nix
   ];
-  # setup secure boot
-  environment.systemPackages = with pkgs; [
-    sbctl
-  ];
-  boot.loader.systemd-boot.enable = lib.mkForce false;
-
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/etc/secureboot";
-  };
   # enable solaar
   programs.solaar.enable = true;
 
---
+++ modules/secureBoot/default.nix
@@ -0,0 +1,14 @@
+{ lib, pkgs, ... }:
+
+{
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
+
+  environment.systemPackages = with pkgs; [
+    sbctl
+  ];
+}
